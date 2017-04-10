aac5c24b608a900b2bc44179b06c51285f9a5016
wansy <wansy23@163.com>
2017-04-10 13:48:24 +0800

    修改解决冲突后的bug

--- src/module/index/vuex/store.js
+++ src/module/index/vuex/store.js
@@ -7,7 +7,7 @@ Vue.use(Vuex)
 const state={
   checkId:'',//全部订单列表点击详情时记住Id
   accountsStatus:'all',  //应收账款中当前选中的状态
-  isBuyer:true   //我是买家
+  isBuyer:true,   //我是买家
   changeRole:'pyee'//pyee卖方，pyer买方
 }
 
